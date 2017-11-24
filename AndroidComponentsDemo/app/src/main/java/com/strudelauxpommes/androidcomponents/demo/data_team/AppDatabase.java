82e1693093feefc6c769a8eda36278e2dba409e6
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-24 10:44:04 -0500

    Do not save the category name, it is provided by the join on an other table

--- AndroidComponentsDemo/app/src/main/java/com/strudelauxpommes/androidcomponents/demo/data_team/AppDatabase.java
+++ AndroidComponentsDemo/app/src/main/java/com/strudelauxpommes/androidcomponents/demo/data_team/AppDatabase.java
@@ -14,7 +14,7 @@ import com.strudelauxpommes.androidcomponents.demo.data_team.converter.*;
  * Created by Marc-Antoine Sauvé on 11/11/17.
  */
 
-@Database(entities = {UIData.class, ActiviteData.class, WeightRecord.class, PrefRecord.class, ActiviteCategory.class}, version = 8)
+@Database(entities = {UIData.class, ActiviteData.class, WeightRecord.class, PrefRecord.class, ActiviteCategory.class}, version = 9)
 @TypeConverters({BackgroundColorConverter.class, CalendarDateConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UIDataDao userDao();
--- AndroidComponentsDemo/app/src/main/java/com/strudelauxpommes/androidcomponents/demo/data_team/record/ActiviteData.java
+++ AndroidComponentsDemo/app/src/main/java/com/strudelauxpommes/androidcomponents/demo/data_team/record/ActiviteData.java
@@ -2,6 +2,7 @@ package com.strudelauxpommes.androidcomponents.demo.data_team.record;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.TypeConverter;
 import android.arch.persistence.room.TypeConverters;
@@ -27,7 +28,7 @@ import static android.arch.persistence.room.ForeignKey.CASCADE;
 public class ActiviteData {
     @NonNull
     private String date;
-    @NonNull
+    @Ignore
     private String activite;
     private int intensite;
     private int categoryId;
